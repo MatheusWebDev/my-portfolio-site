74af51472869e5772fc67d686c385aa35c6a5f2b
Matheus <matheussky.mfj@gmail.com>
2018-05-05 20:36:23 -0300

    Terminou styling header e welcome-area & mixins para layout responsivo

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -5,12 +5,13 @@ cssImport = require('postcss-import'),
 cssNested = require('postcss-nested'),
 autoprefixer = require('autoprefixer'),
 mixins = require('postcss-mixins'),
-hexrgba = require('postcss-hexrgba');
+hexrgba = require('postcss-hexrgba'),
+colorFunction = require("postcss-color-function");
 
 
 gulp.task('styles', function(){
 	return gulp.src('./app/assets/styles/styles.css')
-		.pipe(postcss([cssImport, mixins, cssVars, cssNested, hexrgba, autoprefixer]))
+		.pipe(postcss([cssImport, mixins, cssVars, cssNested, hexrgba, colorFunction, autoprefixer]))
 		.on('error', function(err){
 			console.log(err.toString());
 			this.emit('end');
