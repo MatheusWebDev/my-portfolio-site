b171e49da3571e00ca1c53c6974e49f29edc70fb
Matheus <matheussky.mfj@gmail.com>
2018-05-06 02:19:36 -0300

    welcome-area & header prontos.

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,4 +1,15 @@
 import $ from 'jquery';
 import MobileMenu from './modules/MobileMenu';
+import Typed from 'typed.js';
 
 var mobileMenu = new MobileMenu();
+
+var options = {
+  strings: ["Web Designer", "UX Designer", "Web Developer"],
+  typeSpeed: 80,
+  backSpeed: 50,
+  backDelay: 2000,
+  loop: true
+}
+
+var typed = new Typed("#typed-strings", options);
--- app/assets/scripts/modules/MobileMenu.js
+++ app/assets/scripts/modules/MobileMenu.js
@@ -4,7 +4,7 @@ class MobileMenu {
 	constructor() {
 		this.siteHeader = $(".header");
 		this.menuIcon = $(".header__menu-icon");
-		this.navContent = $(".primary-nav");
+		this.menuContent = $(".header__menu-content");
 		this.events();
 	}
 
@@ -13,7 +13,7 @@ class MobileMenu {
 	}
 
 	troggleTheMenu() {
-		this.navContent.toggleClass("primary-nav--visible");
+		this.menuContent.toggleClass("header__menu-content--visible");
 		this.siteHeader.toggleClass("header--expanded");
 		this.menuIcon.toggleClass("header__menu-icon--close");
 	}
